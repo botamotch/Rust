addea9b4179dd4c6e5ebba68322a3e07a06787e2
bmkazuya <laughing00boomer@gmail.com>
2019-01-02 22:26:51 +0900

    app01; Siganl Processing Program

---
+++ app01/README.md
@@ -0,0 +1,7 @@
+Signal Processing Program
+--------------------------------------------------------------------------------
+
+![Descrete Foureir Tranform](./figure/dft.eps "Descrete Foureir Tranform")
+
+![Inverse Descrete Foureir Tranform](./figure/dft.eps "Inverse Descrete Foureir Tranform")
+
--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,10 +1,12 @@
 extern crate num;
+extern crate gnuplot;
 
 use num::Complex;
+// use gnuplot::{Figure, Caption, Color};
 
 fn main() {
-    let v = vec![1, 2, 3];
-    let x = Complex::new(1, 3);
+    let v = vec![1.1, 1.9, 3.2];
+    let x = Complex::new(1.1, 3.1);
 
     println!(" ==================================================");
     println!("  Hello, world!");
@@ -17,7 +19,7 @@ fn main() {
     for &i in v.iter() {
         let xx = x.scale(i);
         println!("  x      : {}", xx);
-        println!("    norm : {}", xx.norm_sqr());
+        println!("    norm : {}", xx.norm());
         println!("    conj : {}", xx.conj());
     }
     println!(" ==================================================");
