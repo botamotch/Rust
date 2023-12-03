fd30cf77f7d9ddfdd9e288a9a79c55f7a3410c19
Kazuya Buma <laughing00boomer@gmail.com>
2023-12-03 15:28:49 +0900

    update tokio-app - add channel-test.rs

--- tokio-app/examples/channel-test.rs
+++ tokio-app/examples/channel-test.rs
@@ -3,14 +3,14 @@ use mini_redis::client;
 use tokio::sync::{mpsc, oneshot};
 
 #[derive(Debug)]
-enum ChannelCommand {
+enum Command {
   Get {
     key: String,
     resp: Responder<Option<Bytes>>,
   },
   Set {
     key: String,
-    val: Vec<u8>,
+    val: Bytes,
     resp: Responder<()>,
   },
 }
@@ -22,44 +22,56 @@ async fn main() {
   let (tx, mut rx) = mpsc::channel(32);
   let tx2 = tx.clone();
 
-  let manager = tokio::spawn(async move {
-    let mut client = client::connect("127.0.0.1:6379").await.unwrap();
-
-    while let Some(cmd) = rx.recv().await {
-      match cmd {
-        ChannelCommand::Get { key, resp } => {
-          let res = client.get(&key).await;
-          let _ = resp.send(res);
-        }
-        ChannelCommand::Set { key, val, resp } => {
-          let res = client.set(&key, val.into()).await;
-          let _ = resp.send(res);
-        }
-      }
-    }
-  });
-
   let t1 = tokio::spawn(async move {
+    // let _ = tx.send("sending from first handle").await;
     let (resp_tx, resp_rx) = oneshot::channel();
-    let cmd = ChannelCommand::Get {
+    let cmd = Command::Get {
       key: "hello".to_string(),
       resp: resp_tx,
     };
+
     tx.send(cmd).await.unwrap();
     let res = resp_rx.await;
     println!("GOT = {:?}", res);
   });
 
   let t2 = tokio::spawn(async move {
+    // let _ = tx2.send("sending from second handle").await;
     let (resp_tx, resp_rx) = oneshot::channel();
-    let cmd = ChannelCommand::Set {
+    let cmd = Command::Set {
       key: "foo".to_string(),
       val: "bar".into(),
       resp: resp_tx,
     };
+
     tx2.send(cmd).await.unwrap();
     let res = resp_rx.await;
     println!("GOT = {:?}", res);
+  });
+
+  // マネージャタスクの作成
+  // while let Some(message) = rx.recv().await {
+  //   println!("GOT = {}", message);
+  // }
+
+  let manager = tokio::spawn(async move {
+    let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+
+    while let Some(cmd) = rx.recv().await {
+      use Command::*;
+
+      // println!("GOT = {:?}", cmd);
+      match cmd {
+        Get { key, resp } => {
+          let res = client.get(&key).await;
+          let _ = resp.send(res);
+        }
+        Set { key, val, resp } => {
+          let res = client.set(&key, val).await;
+          let _ = resp.send(res);
+        }
+      }
+    }
   });
 
   t1.await.unwrap();
